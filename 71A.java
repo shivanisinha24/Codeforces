faf3f814394674f1f8a6cdb292f44899505d4f55
shivanisinha24 <sinhashivani010@gmail.com>
2026-08-24 10:09:34 +0530

    first commit

---
+++ 71A.java
@@ -0,0 +1,22 @@
+// initialization
+// localization
+
+public class 71A{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int  n = Scanner.nextInt();
+
+    for(int i=0;i<n;i++){
+      String word =scanner.next();
+      if(word.length>10){
+        char first = word.charAt(0);
+        char last = word.length() -1;
+        char middlelen = word.length()-2;
+        System.out.println(first + String.vlaueof(middlelen) + last);
+      }else{
+        System.out.print(word);
+      }
+    }
+    scanner.close();
+  }
+}
---
+++ D.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class D {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    long A = scanner.nextLong();
+    long B = scanner.nextLong();
+    long C = scanner.nextLong();
+    long D = scanner.nextLong();
+    
+    long X =( A * B)- (C * D);
+    System.out.println("Difference = " + X);
+    scanner.close();
+  }
+} 
